6b3ff694e9cc55b6227075d317e4f115e7a1347a
Yash <bavaskaryash1399@gmail.com>
2020-08-22 22:14:17 +0530

    deploy

---
+++ client/barter/build/precache-manifest.2da1fff5ddbb76d489b5f5cf9477c91a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5e7f1e9c02c3e980b4cd5d7bf49cbe9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "084925afb9b2458a7447",
+    "url": "/static/css/2.080caf3a.chunk.css"
+  },
+  {
+    "revision": "2468466dcfdae336520f",
+    "url": "/static/css/main.5ea27863.chunk.css"
+  },
+  {
+    "revision": "084925afb9b2458a7447",
+    "url": "/static/js/2.8199ed4d.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "/static/js/2.8199ed4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2468466dcfdae336520f",
+    "url": "/static/js/main.d9721450.chunk.js"
+  },
+  {
+    "revision": "337bd2ef480524af83ad",
+    "url": "/static/js/runtime-main.499ef68c.js"
+  }
+]);
