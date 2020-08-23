e3eb1b5cb43ff72a6577d1f725b0a3fcef15fee0
Yash <bavaskaryash1399@gmail.com>
2020-08-23 12:16:34 +0530

    correction

--- server/index.js
+++ server/index.js
@@ -34,7 +34,16 @@ app.use('/api/product', require('./routes/product'));
 app.use('/uploads', express.static('uploads'));
 
 // Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
 
+  // Set static folder
+  app.use(express.static("client/barter/build"));
+
+  // index.html for all page routes
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/barter", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000
 
